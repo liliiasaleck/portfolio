fb1cb4567f39e0f0a5b31ba0059211b4f2eff950
Liliia Saleck <lilisaleck@gmail.com>
2021-05-17 14:50:03 +0200

    Add Navbar component, Material UI style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Contact from "./components/Contact";
-import About from "./components/About";
-import Projects from "./components/Projects";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
+    <>
+      <CssBaseline />
       <Home />
-      <Switch>
-        <Route component={Home} path="/" exact />
-        <Route component={About} path="/about" />
-        <Route component={Projects} path="/projects" />
-        <Route component={Contact} path="/contact" />
-      </Switch>
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,96 @@ import React from "react";
 import {
   AppBar,
   Toolbar,
+  ListItem,
   IconButton,
+  ListItemText,
+  Avatar,
+  Divider,
+  List,
   Typography,
-  Button,
+  Box,
+  makeStyles,
+  ListItemIcon,
 } from "@material-ui/core";
-import Menu from "@material-ui/core/Menu";
-import { Link } from "react-router-dom";
+import {
+  ArrowBack,
+  AssignmentInd,
+  Home,
+  Apps,
+  ContactMail,
+} from "@material-ui/icons";
+import avatar from "../images/avatar.png";
+
+const useStyles = makeStyles((theme) => ({
+  sliderContainer: {
+    width: 250,
+    background: "#DC143C",
+    height: "30rem",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+  listItem: {
+    color: "black",
+  },
+}));
+
+const menuItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contacts",
+  },
+];
 
 function Navbar() {
+  const classes = useStyles();
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <Menu />
-          </IconButton>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Liliia Saleck
-          </Typography>
-          <Link to="/about" color="inherit">
-            About
-          </Link>
-          <Link to="/projects" color="inherit">
-            Projects
-          </Link>
-          <Button color="inherit">Contact</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <>
+      <Box className={classes.sliderContainer}>
+        <Avatar className={classes.avatar} src={avatar} alt="Liliia Saleck" />
+        <Divider />
+        <List>
+          {menuItems.map((lsItem, key) => (
+            <ListItem button key={key}>
+              <ListItemIcon className={classes.listItem}>
+                {lsItem.listIcon}
+              </ListItemIcon>
+              <ListItemText
+                className={classes.listItem}
+                primary={lsItem.listText}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "black" }}>
+          <Toolbar>
+            <IconButton>
+              <ArrowBack style={{ color: "white" }} />
+            </IconButton>
+            <Typography variant="h5" style={{ color: "#DC143C" }}>
+              Portfolio
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   );
 }
 
